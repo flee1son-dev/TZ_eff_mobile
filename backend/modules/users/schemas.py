4709cbb76b7e8112b34a7c89c4522f13fbe516dc
foutcast <flee1son@gmail.com>
2025-12-28 16:55:35 +0300

    add update roles

--- backend/modules/users/schemas.py
+++ backend/modules/users/schemas.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field, EmailStr
 from enum import Enum
 from typing import Optional
 
-class UserPermissions(str, Enum):
+class UserRoles(str, Enum):
     director = "director"
     administering = "administering"
     worker = "worker"
@@ -12,7 +12,7 @@ class UserBase(BaseModel):
     email: EmailStr = Field(..., description="User's email")
     first_name: str = Field(..., min_length= 2, max_length=30, description= "User's name" )
     last_name: str = Field(..., min_length= 2, max_length=50, description="User's last name")
-    permissions: UserPermissions = Field(..., description="User's role")
+    permissions: UserRoles = Field(default=UserRoles.worker, description="User's role")
     is_active: bool = Field(default=True, deprecated="active or not active user's account")
 
 
@@ -29,10 +29,11 @@ class UserResponse(UserBase):
 class UserUpdate(BaseModel):
     email: Optional[EmailStr] = Field(default=None, description="User's new email")
     last_name: Optional[str] = Field(default=None, min_length= 2, max_length=50, description="User's new last name")
-    permissions: Optional[UserPermissions] = Field(default=None, description="User's admin status, requires superuser")
     password: Optional[str] = Field(..., min_length=8, max_length=32, description="User's new password")
     password_repeat: Optional[str] = Field(..., min_length=8, max_length=32, description="User's repeat password")
 
+class UserUpdateRoles(BaseModel):
+    permissions: Optional[UserRoles] = Field(default=None, description="New user's role")
     
 class UserDelete(BaseModel):
     is_active: bool = Field(default=False, description="Set to False to deactivate user")

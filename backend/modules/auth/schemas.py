486522eb6e767e74fefee44b6a0bc1a03c030d77
foutcast <flee1son@gmail.com>
2025-12-28 17:13:06 +0300

    Create auth schemas

---
+++ backend/modules/auth/schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, EmailStr, Field
+from backend.modules.users import schemas as userschemas
+
+
+class UserCreate(userschemas.UserCreate):
+    pass
+
+class UserLogin(BaseModel):
+    email: EmailStr = Field(..., description="user's email")
+    password: str = Field(..., description="user's password")
+
+
+class TokenResponse(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "Bearer"

0f114aaae8223de14335044cb982b54c6d1c8c44
foutcast <flee1son@gmail.com>
2025-12-28 15:31:11 +0300

    create access and refresh JWT

--- backend/core/security.py
+++ backend/core/security.py
@@ -2,15 +2,17 @@ from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timedelta
 from passlib.context import CryptContext
 from fastapi import Depends
-from jose import JWTError, jwt
+from jose import JWTError
+import jwt
 from backend.core.config import settings
 from backend.core.database import get_db
+from backend.modules.users import schemas as userschemas
 
 
 SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = settings.ALGORITHM
 ACCESS_TOKEN_EXPIRE = settings.ACCESS_TOKEN_EXPIRES_MINUTES
-REFRESH_TOKEN_EXPIRE = settings.REFRESH_TOKEN_EXPIRES_MINUTES
+REFRESH_TOKEN_EXPIRE = settings.REFRESH_TOKEN_EXPIRES_DAYS
 ACCESS_TOKEN_TYPE = settings.ACCESS_TOKEN_TYPE_FIELD
 REFRESH_TOKEN_TYPE = settings.REFRESH_TOKEN_TYPE_FIELD
 TOKEN_TYPE_FIELD = "type"
@@ -31,7 +33,7 @@ def encode_jwt(
         payload: dict,
         private_key: str = SECRET_KEY, 
         algorithm: str = ALGORITHM,
-        expire_minutes: int, 
+        expire_minutes: int = ACCESS_TOKEN_EXPIRE, 
         expire_days: timedelta | None = None
 ) -> str:
     to_encode = payload.copy()
@@ -45,7 +47,57 @@ def encode_jwt(
         exp=expire,
         iat=now
     )
-    encoded = jwt.encode
+    encoded = jwt.encode(
+        to_encode,
+        private_key,
+        algorithm
+    )
+    return encoded
+
+def create_jwt(
+        type: str, 
+        payload:dict,
+        expire_minutes: int = ACCESS_TOKEN_EXPIRE,
+        expire_days: timedelta | None = None
+):
+    jwt_payload = {TOKEN_TYPE_FIELD: type}
+    jwt_payload.update(payload)
+
+    return encode_jwt(
+        payload=jwt_payload,
+        expire_minutes=expire_minutes,
+        expire_days=expire_days
+    )
+
+def create_access_jwt(user: userschemas.UserBase):
+    jwt_payload = {
+        "sub": user.username,
+        "username": user.username,
+        "email": user.email
+    }
+    return create_jwt(
+        type=ACCESS_TOKEN_TYPE,
+        payload=jwt_payload,
+        expire_minutes=ACCESS_TOKEN_EXPIRE,
+        expire_days= None
+    )
+
+def create_refresh_jwt(user: userschemas.UserBase):
+    jwt_payload = {
+        "sub": user.username,
+        "username": user.username,
+        "email": user.email
+    }
+    return create_jwt(
+        type=REFRESH_TOKEN_TYPE,
+        payload=jwt_payload,
+        expire_days= timedelta(days=REFRESH_TOKEN_EXPIRE)
+    )
+
+
+
+
+
 
 
 
